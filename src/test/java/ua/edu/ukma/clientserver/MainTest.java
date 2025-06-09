7ff8ae1f014ed04f1f51b96e561c1bea49294196
Vladyslav Hibskyi <vgibskiy@gmail.com>
2025-06-10 00:42:40 +0300

    initial commit

---
+++ src/test/java/ua/edu/ukma/clientserver/MainTest.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.clientserver;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void testMain() {
+        assertTrue(true);
+    }
+}
