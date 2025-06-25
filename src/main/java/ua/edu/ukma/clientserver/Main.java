032b11fb5758d4eef18af490755561069212463c
Prokofiev Andrii <prokofiev.main@gmail.com>
2025-06-25 23:05:47 +0300

    added statistics controller

--- src/main/java/ua/edu/ukma/clientserver/Main.java
+++ src/main/java/ua/edu/ukma/clientserver/Main.java
@@ -3,23 +3,26 @@ package ua.edu.ukma.clientserver;
 import com.sun.net.httpserver.HttpServer;
 import ua.edu.ukma.clientserver.controller.GroupController;
 import ua.edu.ukma.clientserver.controller.ProductController;
+import ua.edu.ukma.clientserver.controller.StatisticsController;
 import ua.edu.ukma.clientserver.service.implementation.GroupServiceImpl;
 import ua.edu.ukma.clientserver.service.implementation.ProductServiceImpl;
+import ua.edu.ukma.clientserver.service.implementation.StatisticsServiceImpl;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int port = 8080;
+        InetSocketAddress address = new InetSocketAddress(8080);
 
-        InetSocketAddress address = new InetSocketAddress(port);
         ProductController productController = new ProductController(ProductServiceImpl.getInstance());
         GroupController groupController = new GroupController(GroupServiceImpl.getInstance());
+        StatisticsController statisticsController = new StatisticsController(StatisticsServiceImpl.getInstance());
 
         HttpServer server = HttpServer.create(address, 0);
         server.createContext(ProductController.PRODUCT_PATH, productController);
         server.createContext(GroupController.GROUP_PATH, groupController);
+        server.createContext(StatisticsController.STATISTICS_PATH, statisticsController);
 
         server.start();
     }
---
+++ src/main/java/ua/edu/ukma/clientserver/controller/StatisticsController.java
@@ -0,0 +1,50 @@
+package ua.edu.ukma.clientserver.controller;
+
+import com.sun.net.httpserver.HttpExchange;
+import ua.edu.ukma.clientserver.service.StatisticsService;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+public class StatisticsController extends BaseController {
+
+    public static final String STATISTICS_PATH = "/api/statistics";
+
+    private final StatisticsService productService;
+
+    public StatisticsController(StatisticsService productService) {
+        this.productService = productService;
+    }
+
+    @Override
+    protected void handleGet(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        if (path.matches(STATISTICS_PATH + "/total-price")) {
+            handleGetTotalPrice(exchange);
+        } else if (path.matches(STATISTICS_PATH + "/total-price/group/\\d+")) {
+            handleGetTotalPriceByGroup(exchange);
+        } else {
+            exchange.sendResponseHeaders(404, -1);
+        }
+    }
+
+    private void handleGetTotalPriceByGroup(HttpExchange exchange) throws IOException {
+        String path = exchange.getRequestURI().getPath();
+        Integer groupId = Integer.parseInt(path.substring(path.lastIndexOf('/') + 1));
+        Double totalPrice = productService.getTotalProductsPriceInGroup(groupId);
+        String response = "{\"id:\":" + groupId + ",\"totalPrice\":" + totalPrice + "}";
+        byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        exchange.getResponseBody().write(responseBytes);
+    }
+
+    private void handleGetTotalPrice(HttpExchange exchange) throws IOException {
+        Double totalPrice = productService.getTotalProductsPriceInStorage();
+        String response = "{\"totalPrice\":" + totalPrice + "}";
+        byte[] responseBytes = response.getBytes(StandardCharsets.UTF_8);
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, responseBytes.length);
+        exchange.getResponseBody().write(responseBytes);
+    }
+}
